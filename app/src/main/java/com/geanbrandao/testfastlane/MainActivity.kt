b4468f1df1838d4a1be51cdacad956b12fba4544
Gean Brandao <ctl_gbrandao@uolinc.com>
2021-05-12 15:00:55 -0300

    test

--- app/src/main/java/com/geanbrandao/testfastlane/MainActivity.kt
+++ app/src/main/java/com/geanbrandao/testfastlane/MainActivity.kt
@@ -15,7 +15,5 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
         println("Hello world")
-        println("Hello world")
-        println("Hello world")
     }
 }
